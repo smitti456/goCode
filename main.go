3ecf9f0833d8a677216eb959aad6e0cd486fd2d1
Smitti456 <smittis@gmail.com>
2020-07-06 15:22:34 +0700

    go test about uuid and string function

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
-
 	"gocode/controllers"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 func main() {
@@ -11,4 +13,19 @@ func main() {
 	fmt.Printf("hello, world\n")
 
 	controllers.Hi()
+
+	s := "Hello World"
+	sr := strings.Split(s, " ") // create substring
+	fmt.Println(strings.Contains(s, sr[0]))
+
+	u2, err := uuid.NewV4()
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+		return
+	}
+	fmt.Printf("UUIDv4: %s\n", u2)
+
+	u1 := uuid.Must(uuid.NewV4())
+	fmt.Printf("UUIDv4: %s\n", u1)
+
 }
